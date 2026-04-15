17bf876236b3b1bd82ac61bfa8c24301499607fa
abdullah692 <abd.siddiqui987@gmail.com>
2026-04-16 02:18:33 +0500

    changes

--- middleware/validateToken.js
+++ middleware/validateToken.js
@@ -1,35 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-// const validateToken = async (req, res, next) => {
-//   let authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//     const token = authHeader.split(" ")[1];
-//     console.log("tokenaa",token);
-    
-
-//     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-//       if (err) {
-//         console.log(err, "err:validateToken");
-//         return res.status(406).send({
-//           success: false,
-//           message: "Token is expired or invalid",
-//         });
-//       }
-// console.log("decoded",decoded);
-
-//       req.user = decoded; // Attach decoded user to the request object
-//       next(); // Proceed to the next middleware/controller
-//     });
-//   } else {
-//     return res.status(401).send({
-//       success: false,
-//       message: "Authorization token missing or malformed",
-//     });
-//   }
-// };
-
-
 const validateToken = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
